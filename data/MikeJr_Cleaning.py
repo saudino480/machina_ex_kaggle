574fc4606bb5ffbbc597b7bd0ac64357338d48b0
Michael Drozdov <mdrozdov@dostoyevsky.local>
2019-07-16 21:02:02 -0400

    dealt with missingness for now

--- data/MikeJr_Cleaning.py
+++ data/MikeJr_Cleaning.py
@@ -2,22 +2,36 @@
 import numpy as np
 import pandas as pd
 
+
 ### load data
 train_data = pd.read_csv('./data/test.csv')
 ### subset data
 train_subset = pd.concat([train_data[['Id']],train_data.iloc[:,40:60]],axis=1)
-
-#
-train_subset.isna().sum()
 
 ### Na's to Nones function
 def na_as_none(df, col_list):
     for column in col_list:
         df[column][df[column].isna()] = 'None'
 
-# na's to nones
+### na's to nones
 none_cols = ['FireplaceQu','GarageType']
 na_as_none(train_subset,none_cols)
 
-#
-train_subset[['FireplaceQu']]
+### replaced missing numericals with 0, imputed the most common class for KitchenQual and Functional, 0000 for missing GarageYrBlt to be imputed later
+
+train_subset.BsmtFullBath[train_subset.BsmtFullBath.isna()] = 0
+train_subset.BsmtHalfBath[train_subset.BsmtHalfBath.isna()] = 0
+train_subset.KitchenQual[train_subset.KitchenQual.isna()] = 'TA'
+train_subset.Functional[train_subset.Functional.isna()] = 'Typ'
+train_subset.GarageYrBlt[train_subset.GarageYrBlt.isna()] = 0000
+
+###
+
+for column in train_subset.columns:
+    print(column)
+    print(train_subset[column].unique())
+
+dummy_cols = ['HeatingQC','CentralAir','Electrical','GarageType','KitchenQual',
+                'Functional','FireplaceQu']
+
+dummy_drop_cols = ['TA','N','FuseP','TA','Typ','None']
