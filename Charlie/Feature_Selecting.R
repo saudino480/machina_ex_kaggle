50f3bcb2ae90788977e160e210d32e17e2c837b9
Charles Cohen <iamcharliecohen@gmail.com>
2019-07-22 16:34:56 -0400

    Added Categorical Binning Function to datacleaner.R

--- Charlie/Feature_Selecting.R
+++ Charlie/Feature_Selecting.R
@@ -5,4 +5,32 @@ train <- read.csv('Charlie/datafiles/train_cc_clean.csv')
 test <- read.csv('Charlie/datafiles/test_cc_clean.csv')
 
 
-sapply(train, is.na)
+colSums(is.na(test))
+
+train
+
+## Dummification Test
+model.dummied = lm(SalePrice ~ MSZoning + Street + Neighborhood, data = train)
+summary(model.dummied)
+
+
+# Full Model
+set.seed(0)
+train_split = sample(1:nrow(train), 5*nrow(train)/10)
+train_test = train[-train_split,]
+
+model.full = lm(SalePrice ~ ., data = train, subset = train_split)
+summary(model.full)
+
+pred = predict(model.full, train_test)
+
+
+# Check categorical significance
+for (i in names(total)) {
+  if (is.factor(total[,i])) {
+    print(paste('*****',i,'******'))
+    print((merge(data.frame(table(train[,i])), data.frame(table(test[,i])),by = 'Var1',all=T)))
+  }
+}
+
+table(train[,'Heating'])
--- data/datacleaner.R
+++ data/datacleaner.R
@@ -30,11 +30,34 @@ ready_df <- function(train_df, test_df){
   df$BsmtHalfBath[is.na(df$BsmtCond)] <- 0
   
   
+  df$MasVnrType[(is.na(df$MasVnrType) & !is.na(df$MasVnrArea))] <- 'None'
+  df$MasVnrType[is.na(df$MasVnrType)] = "BrkFace"
+  # Imputed on the Mean value of Areas for Type == 'BrkFace'
+  df$MasVnrArea[is.na(df$MasVnrArea)] = 261.6724
+  
   # MAR: Imputed based on Mode.
   df$Functional[is.na(df$Functional)] <- 'Typ' 
   
   # No Variance in column, imputed on Mode
   df$Utilities[is.na(df$Utilities)] <- 'AllPub' 
+  
+  # Imputed on Mode/Mean of values where GarageType is Detched
+  df$GarageCars[is.na(df$GarageCars)] <- 1
+  df$GarageArea[is.na(df$GarageArea)] <- 419.4923
+  
+  # After YearBuilt = 1970, all Electrical all of type SBrkr
+  df$Electrical[is.na(df$Electrical)] <- 'SBrkr'
+  
+  # MCAR Imputed on Mode
+  df$KitchenQual[is.na(df$KitchenQual)] <- 'TA'
+  
+  # MCAR Imputed on Mode.
+  df$SaleType[is.na(df$SaleType)] <- 'WD'
+  
+  # MAR Imputed on Mode from RoofMatl
+  df$Exterior1st[is.na(df$Exterior1st) & df$RoofMatl =='Tar&Grv'] <- 'CBlock'
+  df$Exterior2nd[is.na(df$Exterior2nd) & df$RoofMatl =='Tar&Grv'] <- 'CBlock'
+  
   
   
   
@@ -45,6 +68,11 @@ ready_df <- function(train_df, test_df){
   df$BsmtExposure <- add_level(df$BsmtExposure)
   df$BsmtFinType2 <- add_level(df$BsmtFinType2)
   df$BsmtFinType1 <- add_level(df$BsmtFinType1)
+  # MAR Replaced with 0, Based on other BSMT Values
+  df$BsmtFinSF1[df$BsmtQual == 'None' & is.na(df$BsmtFinSF1)] <- 0
+  df$BsmtFinSF2[df$BsmtQual == 'None' & is.na(df$BsmtFinSF2)] <- 0
+  df$BsmtFullBath[df$BsmtQual == 'None' & is.na(df$BsmtFullBath)] <- 0
+  
   df$Alley <- add_level(df$Alley)
   df$MiscFeature <- add_level(df$MiscFeature)
   df$Fence <- add_level(df$Fence)
@@ -53,7 +81,6 @@ ready_df <- function(train_df, test_df){
   df$GarageCond <- add_level(df$GarageCond)
   df$GarageFinish <- add_level(df$GarageFinish)
   df$GarageType <- add_level(df$GarageType)
-  df$GarageQual <- add_level(df$GarageQual)
   df$GarageQual <- add_level(df$GarageQual)
   df$GarageYrBlt <- add_level(df$GarageYrBlt, val = 9999)
   
@@ -76,13 +103,30 @@ ready_df <- function(train_df, test_df){
        test_ready = df[1461:2919,])
 }
 
-test <- read.csv("data/test.csv")
-train <- read.csv('data/train.csv')
+## Categorical Binning
+cat_binning <- function(train_df, test_df){
+  df <- rbind(train_df %>% select(-SalePrice),test_df)
+  
+  
+  
+  SalePrice <- train_df$SalePrice
+  sales <- data.frame("SalePrice" = train_df$SalePrice)
+  
+  list(train_binned = cbind(df[1:1460,],sales),
+       test_binned = df[1461:2919,])
+}
 
+
+
+
+test <- read.csv("data/Raw Data/test.csv")
+train <- read.csv('data/Raw Data/train.csv')
 list2env(ready_df(train, test),env=environment())
 
 
+list2env(cat_binning(train_ready,test_ready),env=environment())
 
-apply(is.na(test_ready),2,sum)
-write.csv(x=train_ready,file = 'Charlie/datafiles/train_cc_clean.csv',row.names = F)
-write.csv(x=test_ready,file = 'Charlie/datafiles/test_cc_clean.csv',row.names = F)
+
+
+write.csv(x=train_binned,file = 'Charlie/datafiles/clean_train.csv',row.names = F)
+write.csv(x=test_binned,file = 'Charlie/datafiles/clean_test.csv',row.names = F)
