018827a0224690d45d2ed2a9b2d2883dc0fcc9f4
Charles Cohen <iamcharliecohen@gmail.com>
2019-07-24 22:52:51 -0400

    reset to master

--- mikedollar/FeatureEngineering.R
+++
@@ -1,5 +0,0 @@
-train <- read.csv('../data/clean_train.csv', stringsAsFactors = F)
-test <- read.csv('../data/clean_test.csv', stringsAsFactors = F)
-
-
-fe_drop =c('TotalBsmtSF', 'X1stFlrSF', 'X2ndFlrSF', 'TotRmsAbvGrd', 'LotArea', 'LotFrontage')
