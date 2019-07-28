910f52034b2c2fda3e3e859343d7f7ee7b79d763
Sam A <saudino480@gmail.com>
2019-07-27 23:39:46 -0400

    made stacking function, doesn't really work super well

--- modelling/modelling_functions_new.py
+++ modelling/modelling_functions_new.py
@@ -3,12 +3,140 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression, LassoCV, Lasso, Ridge
+from sklearn.linear_model import LinearRegression, LassoCV, Lasso, Ridge, ElasticNet
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import datasets
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
 from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import accuracy_score, mean_squared_error
+
+
+
+def plot_model_accuracy(df):
+	fig, ax1 = plt.subplots()
+	#ax1.plot(df.index, df['Train Score'],label = 'Train Score')
+	ax1.plot(df.index, df['Test Score'],label='Test Score')
+	ax1.set_xlabel('Model')
+	plt.xticks(rotation=40)
+	ax1.set_ylabel('R2')
+
+	ax2 = ax1.twinx()
+	ax2.plot(df.index, df['Kaggle Metric'], label = 'Kaggle Score',color='blue')
+	ax2.set_ylabel('RMSLE')
+	plt.title('Model Fitness on Test Set')
+	plt.legend(loc=1)
+	plt.show()
+
+def rmsle(real, predicted):
+	sum=0.0
+	for x in range(len(predicted)):
+		if predicted[x]<0 or real[x]<0: #check for negative values
+			continue
+		p = (predicted[x]+1)
+		r = (real[x]+1)
+		sum = sum + (p - r)**2
+	return (sum/len(predicted))**0.5
+
+
+
+def modelStack(training, testing, target, produce_submission = False, n_splits = 5):
+	X_train, X_test, Y_train, Y_test = train_test_split(training, target, test_size = 0.2, random_state=42)
+
+	skf = KFold(n_splits, random_state = None, shuffle = True)
+	skf.get_n_splits(X_train, Y_train)
+
+	X_train_set = []
+	X_test_set = []
+	y_train_set = []
+	y_test_set = []
+	#index_train_set = []
+	#index_test_set = []
+
+	for train_index, test_index in skf.split(X_train, Y_train):
+		#print("TRAINING:", train_index, "TEST:", test_index)
+		#index_train_set.append(train_index)
+		#index_test_set.append(test_index)
+		X_train_set.append(X_train.iloc[train_index])
+		X_test_set.append(X_train.iloc[test_index])
+		y_train_set.append(Y_train.iloc[train_index])
+		y_test_set.append(Y_train.iloc[test_index])
+
+	#index_train_set = [x.tolist() for x in index_train_set]
+
+	#print(len(X_train_set))
+	pred_train_dict = {}
+	pred_test_dict = {}
+	predict_final = {}
+
+	alpha_steps = (1e-5,1e-2,200)
+	steps = np.linspace(1,0.8,21)
+	lasso = Lasso()
+	ridge = Ridge()
+	elasticnets = [ElasticNet(l1_ratio = i) for i in steps[1:]]
+	names = ['Lasso'] + ['EN_' + str(round(i,2)) for i in steps[1:]]
+	modelList = pd.Series([lasso] + elasticnets, index = names)
+
+	for i in range(len(X_train_set)):
+		print(i)
+		print(X_train_set[i].shape)
+		print('Tuning Hyperparameters...')
+		param_grid = {'max_iter': [10,100,1000],
+					'alpha':np.linspace(1e-5,1e-3,100)}
+		grid = GridSearchCV(modelList.Lasso,param_grid,scoring='r2',cv=5)
+		grid.fit(X_train_set[i], y_train_set[i])
+		best_alpha = grid.best_params_['alpha']
+		best_iter = grid.best_params_['max_iter']
+		print('Best Alpha Found: {}\n'.format(best_alpha))
+		modelList.apply(lambda x: x.set_params(alpha = best_alpha, max_iter = best_iter))
+
+		modelList.apply(lambda x: x.fit(X_train_set[i], y_train_set[i]))
+		train_errors, test_errors = [], []
+		train_errors.append(modelList.apply(lambda x: x.score(X_train_set[i], y_train_set[i])))
+		test_errors.append(modelList.apply(lambda x: x.score(X_train_set[i], y_train_set[i])))
+		scores_df = pd.DataFrame({"Train Score":modelList.apply(lambda x: x.score(X_train_set[i], y_train_set[i])),
+						 "Test Score": modelList.apply(lambda x: x.score(X_train_set[i], y_train_set[i])),
+						 "Kaggle Metric": modelList.apply(lambda x: rmsle(y_test_set[i].values, x.predict(X_test_set[i])))
+						})
+
+		plot_model_accuracy(scores_df)
+		np.argmin(scores_df['Kaggle Metric'])
+		print('Best Model: ', np.argmin(scores_df['Kaggle Metric']))
+		print('Kaggle Score: ', round(min(scores_df['Kaggle Metric']),4))
+
+		print('')
+		model = modelList[np.argmin(scores_df['Kaggle Metric'])].fit(X_train_set[i], y_train_set[i])
+
+		#model.fit(X_train_set[i], y_train_set[i])
+		for j in range(i + 1, len(X_train_set)):
+			print(j)
+			pred_train = model.predict(X_train_set[j])
+			pred_test = model.predict(X_test_set[j])
+			df_train = pd.DataFrame(pred_train, index = X_train_set[j].index)
+			df_test = pd.DataFrame(pred_test, index = X_test_set[j].index)
+			#df_final = pd.DataFrame(model.predict(training), index = X_train_set[j].index)
+			#print(df.index == X_train_set[j].index)
+			#print((X_train_set[j].head()))
+			if (j == (i+1)):
+				df_X_test = pd.DataFrame(model.predict(X_test), index = X_test.index)
+				df_training = pd.DataFrame(model.predict(training), index = training.index)
+				df_testing = pd.DataFrame(model.predict(testing), index = testing.index)
+
+				print(mean_squared_error(Y_test, model.predict(X_test)))
+
+				X_test = pd.concat([X_test, df_X_test], axis = 1, join_axes = [X_test.index])
+				training = pd.concat([training, df_training], axis = 1, join_axes = [training.index])
+				testing = pd.concat([testing, df_testing], axis = 1, join_axes = [testing.index])
+
+			X_train_set[j] = pd.concat([X_train_set[j], df_train], axis = 1, join_axes = [X_train_set[j].index], join = 'inner')
+			X_test_set[j] = pd.concat([X_test_set[j], df_test], axis = 1, join_axes = [X_test_set[j].index], join = 'inner')
+			print((X_train_set[j]).shape)
+
+
+	if (produce_submission):
+		return np.exp(model.predict(testing))
+
+
+
 
 def to_numeric(df, col, target="", test = False):
 	"""Refactors levels to numerals in order of increasing median sale price
@@ -90,7 +218,7 @@ def read_and_clean(filepath, test = False):
 		#num_colname = list(needs_numeric.columns)
 		trans_colname = list(needs_tranform.columns)
 		trim = ['BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
-		 		'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+				'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
 				'GarageYrBlt', 'GarageCars', 'MiscVal', 'MoSold', 'YrSold',
 				'MSSubClass']
 		trans_colname = [x for x in trans_colname if x not in trim]
@@ -119,7 +247,7 @@ def read_and_clean(filepath, test = False):
 		#### Read Data files
 		housing = pd.read_csv(filepath)
 		outliers = [198,  524,  589,  692,
-            		826, 1183, 1299, 1424]
+					826, 1183, 1299, 1424]
 		housing = housing[~housing["Id"].isin(outliers)]
 		housing = housing.drop('Id', axis=1)
 		### Identifies columns by type obj
@@ -128,7 +256,7 @@ def read_and_clean(filepath, test = False):
 		#num_colname = list(needs_numeric.columns)
 		trans_colname = list(needs_tranform.columns)
 		trim = ['BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
-		 		'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+				'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
 				'GarageYrBlt', 'GarageCars', 'MiscVal', 'MoSold', 'YrSold',
 				'MSSubClass']
 		trans_colname = [x for x in trans_colname if x not in trim]
@@ -151,7 +279,7 @@ def read_and_clean(filepath, test = False):
 		housing_features = housing.drop(['saleprice'], axis=1)
 		feat_labels = housing_features.columns
 		#housing.saleprice = np.log(housing.saleprice)
-        
+
 		return housing, housing_features, feat_labels#, dict_dictonary
 
 
@@ -266,10 +394,10 @@ def optimize_penalty(features, target, model = Lasso, min_=0,max_=10, step=0.01,
 	return coefs_, term_, R2_
 
 def undo_transform(train, test, col, transform):
-    if (transform == "sqrt"):
-        train[col] = train[col]**2
-        test[col] = test[col]**2
-    elif (transform == "log"):
-        train[col] = np.exp(train[col])
-        test[col] = np.exp(test[col])
-    #return train[col], test[col]
+	if (transform == "sqrt"):
+		train[col] = train[col]**2
+		test[col] = test[col]**2
+	elif (transform == "log"):
+		train[col] = np.exp(train[col])
+		test[col] = np.exp(test[col])
+	#return train[col], test[col]
